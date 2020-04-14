64ed3cce9f187103f3695ff8c11fbbca4fbae1e3
Wendy Su <w696905@gmail.com>
2020-04-14 11:06:52 +0100

    user defines the start and the end of the range of random numbers

--- number_guessing.py
+++ number_guessing.py
@@ -1,17 +1,22 @@
 import random
+start = input('Please enter the start of the random range: ')
+end = input('Please enter the end of the random range: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 times = 0
 while True:
-	guess = input('Please type a number between 1 and 100: ')
+	guess = input('Please type a number between the start and the end range: ')
 	guess = int(guess)
+	times = times + 1
 	if guess == r:
 		print('Correct guess!')
+		print('This is the', times, 'times guessing.')
 		break
 	else:
 		if guess > r:
 			print('r is smaller.')
 		elif guess < r:
 			print('r is bigger.')
-	times = times + 1
 	print('This is the', times, 'times guessing.')
