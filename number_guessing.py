f80d197281b348c99882c8665534859157e03ba1
Wendy Su <w696905@gmail.com>
2020-04-14 10:32:25 +0100

    add guessing times

--- number_guessing.py
+++ number_guessing.py
@@ -1,6 +1,7 @@
 import random
 
 r = random.randint(1, 100)
+times = 0
 while True:
 	guess = input('Please type a number between 1 and 100: ')
 	guess = int(guess)
@@ -12,3 +13,5 @@ while True:
 			print('r is smaller.')
 		elif guess < r:
 			print('r is bigger.')
+	times = times + 1
+	print('This is the', times, 'times guessing.')
